0a0254f6f955682193e2d7e49def5149c933d49e
LLMR_Boringtao <138439682+LLMR-boringtao@users.noreply.github.com>
2024-01-16 22:49:08 +0000

    modify: header

---
+++ azd.sh
@@ -0,0 +1,11 @@
+ #!/bin/sh
+ # https://app-backend-drpvjxoz6oigo.azurewebsites.net/
+
+ azd env set AZURE_RESOURCE_GROUP
+ azd env set AZURE_LOCATION
+ azd env set AZURE_OPENAI_SERVICE
+ azd env set AZURE_OPENAI_CHATGPT_DEPLOYMENT
+ azd env set AZURE_OPENAI_EMB_DEPLOYMENT
+ azd env set AZURE_OPENAI_EMB_DEPLOYMENT
+ azd env set AZURE_SEARCH_SERVICE_RESOURCE_GROUP
+ azd env set AZURE_SEARCH_SERVICE_RESOURCE_GROUP
